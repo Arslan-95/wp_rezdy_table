cabaeacf6ace5f7aebe60a793e8cb350e05ba6c7
Arslan-95 <66091159+Arslan-95@users.noreply.github.com>
2022-01-14 22:00:49 +0300

    Add files via upload

--- rezdy-agent-table.php
+++ rezdy-agent-table.php
@@ -1,6 +1,11 @@
 <?php
 /*
 Plugin Name: Rezdy Agent Table
+Description: Plugin for request and display data from Rezdy Api.
+Version: 1.0
+Author: Arslan Sarakaev
+Author URI: https://www.instagram.com/arslan_sarakaev/
+Plugin URI: https://kwork.ru/user/arslansarakaev
 */
 include_once(__DIR__ . '/includes/admin-menu.php');
 include_once(__DIR__ . '/includes/admin-submenu.php');
@@ -13,8 +18,9 @@ function rezdy_agent_table_activate(){
   add_option('rezdy_names', []);
 }
 
-register_deactivation_hook( __FILE__, 'rezdy_agent_table_deactivate' );
+register_uninstall_hook( __FILE__, 'rezdy_agent_table_uninstall' );
 
-function rezdy_agent_table_deactivate(){
+function rezdy_agent_table_uninstall(){
   delete_option('rezdy_apiKey');
+  delete_option('rezdy_names');
 }
